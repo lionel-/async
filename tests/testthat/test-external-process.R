9f00f54eb9ea279da907709961def4c8b71847c8
Lionel Henry <lionel.hry@gmail.com>
2023-03-27 15:23:20 +0200

    Support process connections in `external_process()`

--- tests/testthat/test-external-process.R
+++ tests/testthat/test-external-process.R
@@ -78,3 +78,24 @@ test_that("discarding stdout/stderr works", {
   expect_null(res$stderr)
   expect_false(res$timeout)
 })
+
+test_that("can create processes with output connections", {
+  px <- asNamespace("processx")$get_tool("px")
+  pxgen <- function(...) {
+    processx::process$new(
+      px,
+      c("outln", "foo", "errln", "bar"),
+      stdout = "|",
+      stderr = "2>&1",
+      ...
+    )
+  }
+
+  afun <- function() external_process(pxgen)
+
+  res <- synchronise(afun())
+  expect_equal(res$status, 0L)
+  expect_match(res$stdout, "foo\r?\nbar")
+  expect_null(res$stderr)
+  expect_false(res$timeout)
+})
